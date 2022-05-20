5e7c407b1fede59d9bbaf4ecd7527dd783f5e366
ikeofilic1 <62239576+ikeofilic1@users.noreply.github.com>
2022-05-20 16:12:00 -0500

    Update CountSort.java

--- sorting algorithms/sort/CountSort.java
+++ sorting algorithms/sort/CountSort.java
@@ -2,42 +2,52 @@ package sort;
 
 public class CountSort {
 
-	/*public static Object[] sort(Object[] arr, Object base) {
-		CompareClass integerComp = (a, b) -> (int)b - (int)a;
-		return sort(arr, base, arr.length, arr.length, integerComp);		
+	/*public static <T extends Number> T[] sort(T[] arr) {
+		T max = arr[0], min = max;
+        for (int i = 1; i < arr.length; ++i) {
+            if (arr[i] < min) min = arr[i];
+            else if (arr[i] > max) max = arr[i];
+        }
+        int keys = (max-min+1).intValue();
+        if (arr.length < keys) keys = arr.length;
+		return sort(arr, min, keys);		
 	}
+    
+    public static <T extends Comparable<? super T>> T[] sort(T[] arr) {
+        T base = arr[0];
+        for (int i = 1; i < arr.length; ++i)
+            if (base.compareTo(arr[i]) > 0) base = arr[i];
+        
+        return sort(arr, base, arr.length);
+    }
 
-	public static Object[] sort(Object[] arr, Object base, int size, int keys, CompareClass func) {
-		if (base == null) throw new NullPointerException(); 
+	private static T[] sort(T[] arr, T base, int keys) {
+		if (arr == null) throw new NullPointerException(); 
 
 		int[] counts = new int[keys];
-		Object[] temp = new Object[size];
+		T[] temp = new T[size];
 
 		for(int j = 0; j < keys; j++)
 			counts[j] = 0;
 
 		for(int t = 0; t < size; t++)
-			++counts[func.compare(base, arr[t])];
+			++counts[base.compareTo(arr[t])];
 		
 		for(int j = 1; j < keys; j++) 
 			counts[j] += counts[j-1];
 
 		for(int t = size; t > 0; t--)
-			temp[(--counts[func.compare(base, arr[t-1])])] = arr[t-1];
+			temp[(--counts[base.compare(arr[t-1])])] = arr[t-1];
 
 		return temp;
 	}*/
 
-	public static Object[] sort(Integer[] arr, int keys) {
-        return sort(arr, keys, b -> (int)b);      
-    }
-
-    public static Object[] sort(Object[] arr, int keys, MyInt func) {
+	public static <T extends Object> T[] sort(T[] arr, int keys, MyInt func) {
         int[] mm = mm(arr, func);
         int min = mm[0], max = mm[1];
 
         int[] counts = new int[keys];
-        Object[] temp = new Object[arr.length];
+        T[] temp = new T[arr.length];
 
         for(int j = 0; j < keys; j++)
             counts[j] = 0;
